645d83373107de43870136fa92a5bf6d2f39dd5d
Lily Zhu <lszhu00@gmail.com>
2017-04-12 12:48:42 -0400

    node

---
+++ server/src/helloworld.js
@@ -0,0 +1,7 @@
+// Import 'reverseString'
+var Util = require('./util');
+var reverseString = Util.reverseString;
+
+var reversedString = reverseString("Hello World!");
+
+console.log(reversedString);
